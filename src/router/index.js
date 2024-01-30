b76547c918cab0865536830c3385fdbe98a94730
YBARAKA1 <jeffbaraka1@gmail.com>
2024-01-30 18:09:56 +0300

    mealsearch

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import MealList from '../views/MealList.vue'
+const routes = [{
+    path: '/',
+    name: 'home',
+    component: Home,
+},
+{
+    path: '/letter/:letter',
+    name: 'byletter',
+    component: MealList,
+},
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
